b5c7333f69dc8ed9fb930e53301ccb56e78d916a
Abhay955528 <123722051+Abhay955528@users.noreply.github.com>
2026-02-22 23:48:10 +0530

    update_1

--- src/components/HeaderItem.jsx
+++ src/components/HeaderItem.jsx
@@ -5,7 +5,7 @@ function HeaderItem({name,Icon}) {
   return (
     <div className='text-white flex items-center gap-3 text-[15px] font-semibold cursor-pointer hover:underline
     underline-offset-8 mb-2'>
-        < Icon className='text-xl'/>
+        < Icon className='text-lg'/>
         <h2 className=''>{name}</h2>
     </div>
   )
